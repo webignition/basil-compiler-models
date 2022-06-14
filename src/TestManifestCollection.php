96fc5a7a656ae0b19f67d757cba894834ce3b81b
Jon Cram <webignition@gmail.com>
2022-06-14 15:21:46 +0100

    Create TestManifestCollection (#69)

---
+++ src/TestManifestCollection.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilCompilerModels;
+
+class TestManifestCollection implements OutputInterface
+{
+    /**
+     * @var TestManifest[]
+     */
+    private array $testManifests = [];
+
+    /**
+     * @param array<mixed> $testManifests
+     */
+    public function __construct(array $testManifests)
+    {
+        foreach ($testManifests as $testManifest) {
+            if ($testManifest instanceof TestManifest) {
+                $this->testManifests[] = $testManifest;
+            }
+        }
+    }
+
+    /**
+     * @return TestManifest[]
+     */
+    public function getManifests(): array
+    {
+        return $this->testManifests;
+    }
+
+    public function toArray(): array
+    {
+        $data = [];
+
+        foreach ($this->testManifests as $testManifest) {
+            $data[] = $testManifest->toArray();
+        }
+
+        return $data;
+    }
+
+    public static function fromArray(array $data): OutputInterface
+    {
+        $testManifests = [];
+
+        foreach ($data as $value) {
+            if (is_array($value)) {
+                $testManifest = TestManifest::fromArray($value);
+
+                if (TestManifest::VALIDATION_STATE_VALID === $testManifest->validate()) {
+                    $testManifests[] = $testManifest;
+                }
+            }
+        }
+
+        return new TestManifestCollection($testManifests);
+    }
+}
